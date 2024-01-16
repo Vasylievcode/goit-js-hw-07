b47ea855f99343b970640ae513ab152e43fb0364
Vasylievcode <133871835+Vasylievcode@users.noreply.github.com>
2024-01-16 10:19:14 +0000

    1 Done

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,7 @@
 const numberOfCategoriesEl = document.querySelectorAll('.item')
 console.log(`Number of categories:`, numberOfCategoriesEl.length)
+numberOfCategoriesEl.forEach((categoryEl)=> {
+    const headerEl = categoryEl.querySelector('h2').textContent
+    console.log('Category:' , headerEl)
+    console.log('Elements:' , categoryEl.querySelectorAll('li').length)
+})
